b88174e165a36d7fb8faca0356c587862e159702
yiy82 <yiy82@DESKTOP-19JJ6NI>
2019-12-25 18:20:50 +0900

    2019-12-24-v1

--- src/kr/co/acorn/dao/MemberDao.java
+++ src/kr/co/acorn/dao/MemberDao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import kr.co.acorn.dto.HelpMeDto;
+import kr.co.acorn.dto.MemberDto;
 import kr.co.acorn.util.ConnLocator;
 
 public class MemberDao {
@@ -107,8 +107,8 @@ public class MemberDao {
 		return isSuccess;
 	}
 
-	public ArrayList<HelpMeDto> select(int start, int len) {
-		ArrayList<HelpMeDto> list = new ArrayList<HelpMeDto>();
+	public ArrayList<MemberDto> select(int start, int len) {
+		ArrayList<MemberDto> list = new ArrayList<MemberDto>();
 
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -117,13 +117,13 @@ public class MemberDao {
 		try {
 			con = ConnLocator.getConnection();
 			StringBuffer sql = new StringBuffer();
-			sql.append("SELECT category, title, content, gender, helper_email, ask_email, iscomplete, ask_addr, DATE_FORMAT(regdate, '%Y/%m/%d') ");
-			sql.append("FROM help_me ");
+			sql.append("SELECT email, name, pwd, phone, addr, gender, DATE_FORMAT(regdate, '%Y/%m/%d') ");
+			sql.append("FROM member ");
 			sql.append("ORDER BY regdate DESC ");
 			sql.append("LIMIT ?, ? ");//
-			
+
 			System.out.println(con);
-			
+
 			pstmt = con.prepareStatement(sql.toString());
 			int index = 0;
 			pstmt.setInt(++index, start);
@@ -131,15 +131,15 @@ public class MemberDao {
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				index = 0;
-				int category = rs.getInt(++index);
-				
-				  String title = rs.getString(++index); String content = rs.getString(++index);
-				  int gender = rs.getInt(++index); String helper_email = rs.getString(++index);
-				  String ask_email = rs.getString(++index); boolean iscomplete =
-				  rs.getBoolean(++index); String ask_addr = rs.getString(++index); 
-				  String regdate = rs.getString(++index);
+				String email = rs.getString(++index);
+				String name = rs.getString(++index);
+				String password = rs.getString(++index);
+				String phone = rs.getString(++index);
+				String addr = rs.getString(++index);
+				int gender = rs.getInt(++index);
+				String regdate = rs.getString(++index);
 
-				list.add(new HelpMeDto(category, title, content, gender, helper_email, ask_email, iscomplete, ask_addr, regdate)); 
+				list.add(new MemberDto(email, name, password, phone, addr, gender, regdate));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -161,144 +161,202 @@ public class MemberDao {
 
 		return list;
 	}
+
+	public MemberDto select(String email) {
+		MemberDto dto = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT email, name, pwd, phone, addr, gender, DATE_FORMAT(regdate, '%Y/%m/%d') ");
+			sql.append("FROM member ");
+			sql.append("WHERE email = ? ");
+
+			pstmt = con.prepareStatement(sql.toString());
+			int index = 0;
+			pstmt.setString(++index, email);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				email = rs.getString(++index);
+				String name = rs.getString(++index);
+				String password = rs.getString(++index);
+				String phone = rs.getString(++index);
+				String addr = rs.getString(++index);
+				int gender = rs.getInt(++index);
+				String regdate = rs.getString(++index);
+
+				dto = new MemberDto(email, name, password, phone, addr, gender, regdate);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return dto;
+	}
+
+	public boolean insert(MemberDto dto) {
+		boolean isSuccess = false;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("INSERT INTO member(email, name, pwd, phone, addr, gender, regdate) ");
+			sql.append("VALUES(?, ?, ?, ?, ?, ?, CURDATE()) ");
+
+			pstmt = con.prepareStatement(sql.toString());
+			int index = 0;
+			pstmt.setString(++index, dto.getEmail());
+			pstmt.setString(++index, dto.getName());
+			pstmt.setString(++index, dto.getPassword());
+			pstmt.setString(++index, dto.getPhone());
+			pstmt.setString(++index, dto.getAddr());
+			pstmt.setInt(++index, dto.getGender());
+			pstmt.executeUpdate();
+
+			isSuccess = true;
+
+		} catch (SQLException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+
+			} catch (SQLException e) { // TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return isSuccess;
+	}
+
+	public boolean update(MemberDto dto) {
+		boolean isSuccess = false;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("UPDATE member ");
+			sql.append("SET name = ?, pwd = ?, phone = ?, addr = ?, gender = ? ");
+			sql.append("WHERE email = ? ");
+
+			pstmt = con.prepareStatement(sql.toString());
+			int index = 0;
+			pstmt.setString(++index, dto.getName());
+			pstmt.setString(++index, dto.getPassword());
+			pstmt.setString(++index, dto.getPhone());
+			pstmt.setString(++index, dto.getEmail());
+
+			pstmt.executeUpdate();
+
+			isSuccess = true;
+
+		} catch (SQLException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+
+			} catch (SQLException e) { // TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return isSuccess;
+	}
+
+	public boolean delete(String email) {
+		boolean isSuccess = false;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("DELETE FROM member WHERE email = ? ");
+
+			pstmt = con.prepareStatement(sql.toString());
+			int index = 0;
+			pstmt.setString(++index, email);
+
+			pstmt.executeUpdate();
+
+			isSuccess = true;
+
+		} catch (SQLException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+
+			} catch (SQLException e) { // TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return isSuccess;
+	}
+
+	public MemberDto getMember(MemberDto dto) { MemberDto MemberDto = null;
+  
+  Connection con = null; PreparedStatement pstmt = null; ResultSet rs = null;
+  
+  try { con = ConnLocator.getConnection(); StringBuffer sql = new
+  StringBuffer(); sql.
+  append("SELECT m_email, m_name, m_phone, date_format(m_regdate, '%Y/%m/%d') "
+  ); sql.append("FROM member ");
+  sql.append("WHERE m_email = ? AND m_pwd = PASSWORD(?) ");
+  
+  pstmt = con.prepareStatement(sql.toString()); int index = 0;
+  pstmt.setString(++index, dto.getEmail() ); pstmt.setString(++index,
+  dto.getPassword());
+  
+  rs = pstmt.executeQuery(); if (rs.next()) { index = 0; String email =
+  rs.getString(++index); String name = rs.getString(++index); String phone =
+  rs.getString(++index); String regdate = rs.getString(++index); MemberDto =
+  new MemberDto(email, name, null, phone, regdate); } } catch (SQLException e)
+  { // TODO Auto-generated catch block e.printStackTrace(); } finally { try {
+  if (rs != null) rs.close(); if (pstmt != null) pstmt.close(); if (con !=
+  null) con.close();
+  
+  } catch (SQLException e) { // TODO Auto-generated catch block
+  e.printStackTrace(); } }
+
+	return MemberDto;
+
 }
-/*
- * public HelpMeDto select(String email) { HelpMeDto dto = null;
- * 
- * Connection con = null; PreparedStatement pstmt = null; ResultSet rs = null;
- * 
- * try { con = ConnLocator.getConnection(); StringBuffer sql = new
- * StringBuffer(); sql.
- * append("SELECT m_email, m_name, m_pwd, m_phone, DATE_FORMAT(m_regdate, '%Y/%m/%d') "
- * ); sql.append("FROM member "); sql.append("WHERE m_email = ? ");
- * 
- * pstmt = con.prepareStatement(sql.toString()); int index = 0;
- * pstmt.setString(++index, email);
- * 
- * rs = pstmt.executeQuery(); if (rs.next()) { index = 0; email =
- * rs.getString(++index); String name = rs.getString(++index); String pwd =
- * rs.getString(++index); String phone = rs.getString(++index); String regdate =
- * rs.getString(++index);
- * 
- * DeptDto deptDto = new DeptDto(deptNo, null, null); // null�� name, loc
- * 
- * dto = new HelpMeDto(email, name, pwd, phone, regdate); } } catch
- * (SQLException e) { // TODO Auto-generated catch block e.printStackTrace(); }
- * finally { try { if (rs != null) rs.close(); if (pstmt != null) pstmt.close();
- * if (con != null) con.close();
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } }
- * 
- * return dto; }
- * 
- * public boolean insert(HelpMeDto dto) { boolean isSuccess = false;
- * 
- * Connection con = null; PreparedStatement pstmt = null;
- * 
- * try { con = ConnLocator.getConnection(); StringBuffer sql = new
- * StringBuffer();
- * sql.append("INSERT INTO member(m_email, m_name, m_pwd, m_phone, m_regdate) "
- * ); sql.append("VALUES(?, ?, ?, ?, CURDATE()) ");
- * 
- * 
- * pstmt = con.prepareStatement(sql.toString()); int index = 0;
- * pstmt.setString(++index, dto.getEmail()); pstmt.setString(++index,
- * dto.getName()); pstmt.setString(++index, dto.getPassword());
- * pstmt.setString(++index, dto.getPhone());
- * 
- * pstmt.executeUpdate();
- * 
- * isSuccess = true;
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } finally { try { if (pstmt != null) pstmt.close(); if
- * (con != null) con.close();
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); }
- * 
- * } return isSuccess; }
- * 
- * public boolean update(HelpMeDto dto) { boolean isSuccess = false;
- * 
- * Connection con = null; PreparedStatement pstmt = null;
- * 
- * try { con = ConnLocator.getConnection(); StringBuffer sql = new
- * StringBuffer(); sql.append("UPDATE member ");
- * sql.append("SET m_name = ?, m_pwd = ?, m_phone = ? ");
- * sql.append("WHERE m_email = ? ");
- * 
- * pstmt = con.prepareStatement(sql.toString()); int index = 0;
- * pstmt.setString(++index, dto.getName()); pstmt.setString(++index,
- * dto.getPassword()); pstmt.setString(++index, dto.getPhone());
- * pstmt.setString(++index, dto.getEmail());
- * 
- * pstmt.executeUpdate();
- * 
- * isSuccess = true;
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } finally { try { if (pstmt != null) pstmt.close(); if
- * (con != null) con.close();
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); }
- * 
- * } return isSuccess; }
- * 
- * public boolean delete(String email) { boolean isSuccess = false;
- * 
- * Connection con = null; PreparedStatement pstmt = null;
- * 
- * try { con = ConnLocator.getConnection(); StringBuffer sql = new
- * StringBuffer(); sql.append("DELETE FROM member WHERE m_email = ? ");
- * 
- * pstmt = con.prepareStatement(sql.toString()); int index = 0;
- * pstmt.setString(++index, email);
- * 
- * pstmt.executeUpdate();
- * 
- * isSuccess = true;
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } finally { try { if (pstmt != null) pstmt.close(); if
- * (con != null) con.close();
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } }
- * 
- * return isSuccess; }
- * 
- * public HelpMeDto getMember(HelpMeDto dto) { HelpMeDto HelpMeDto = null;
- * 
- * Connection con = null; PreparedStatement pstmt = null; ResultSet rs = null;
- * 
- * try { con = ConnLocator.getConnection(); StringBuffer sql = new
- * StringBuffer(); sql.
- * append("SELECT m_email, m_name, m_phone, date_format(m_regdate, '%Y/%m/%d') "
- * ); sql.append("FROM member ");
- * sql.append("WHERE m_email = ? AND m_pwd = PASSWORD(?) ");
- * 
- * pstmt = con.prepareStatement(sql.toString()); int index = 0;
- * pstmt.setString(++index, dto.getEmail() ); pstmt.setString(++index,
- * dto.getPassword());
- * 
- * rs = pstmt.executeQuery(); if (rs.next()) { index = 0; String email =
- * rs.getString(++index); String name = rs.getString(++index); String phone =
- * rs.getString(++index); String regdate = rs.getString(++index); HelpMeDto =
- * new HelpMeDto(email, name, null, phone, regdate); } } catch (SQLException e)
- * { // TODO Auto-generated catch block e.printStackTrace(); } finally { try {
- * if (rs != null) rs.close(); if (pstmt != null) pstmt.close(); if (con !=
- * null) con.close();
- * 
- * } catch (SQLException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } }
- * 
- * return HelpMeDto;
- * 
- * }
- * 
- * 
- * }
- */
+
+}}
